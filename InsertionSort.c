869722ee71e0e138f0a585871a3c081a76f3906f
Ashik s <ashikpersonal777@gmail.com>
2025-11-30 22:57:15 +0530

    Create InsertionSort.c

---
+++ InsertionSort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+void Selectionsort(int arr[],int n)
+{    int temp,x;
+    for(int i=0;i<n;i++)
+    {  x=arr[i];
+        int j=i-1;
+        while(j>-1&&arr[j]>x)
+        { arr[j+1]=arr[j];j--;}
+                    arr[j+1]=x;
+
+    }
+    printf("\nAfter sorting\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+}
+int main(){
+    int i,j;
+    int n;
+    printf("Enter the number of terms: ");
+    scanf("%d",&n);
+int arr[n];
+printf("\nEnter elements: ");
+for(i=0;i<n;i++)
+{
+    scanf("%d",&arr[i]);
+}
+Selectionsort(arr,n);
+return 0;
+
+}
